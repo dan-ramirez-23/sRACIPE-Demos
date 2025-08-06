db2d5e42d302f9560cbd6bfb73bc9dc654cd2622
Dan Ramirez <danramirez@Dans-Mac-mini.local>
2025-08-06 16:46:17 -0400

    unique state ID & toggle switch files

--- R/circuit_demos.R
+++ R/circuit_demos.R
@@ -1,9 +1,11 @@
 ##### SETUP & GLOBALS #####
 rm(list=ls())
 #devtools::install_github("lusystemsbio/sRACIPE")
-library(sRACIPE)
-library(ggplot2)
-library(microbenchmark)
+library(sRACIPE)                 # GRN simulation
+library(ggplot2)                 # plotting
+library(microbenchmark)          # time cost benchmarking
+library(dplyr)                   # data management
+
 
 # seed for reproducibility & color palette for plots
 set.seed(1234)
@@ -13,6 +15,11 @@ cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 topo_list <- c("toggleSwitch", "CTS", "TS_rep_2", "cellcycle")
 k_list <- c(2, 4, 3, 6) # expected number of clusters for each topo
 names(k_list) <- topo_list
+
+nModels <- 1000
+nICs <- 100
+n_places_rounded <- 4
+
 
 ##### SIMULATION & EDA ON UNPERTURBED NETWORK #####
 for(topo_name in topo_list) {
@@ -39,7 +46,7 @@ for(topo_name in topo_list) {
   racipe_fname <- file.path(dataDir,"racipe.Rds")
   if(!file.exists(mb_rs_fname)) {
     mb_rs <- microbenchmark({
-      racipe <- sracipeSimulate(topo, numModels = 1000, nIC = 100)
+      racipe <- sracipeSimulate(topo, numModels = nModels, nIC = nICs)
     }, times = 10)
     saveRDS(mb_rs, mb_rs_fname)
     saveRDS(racipe, racipe_fname)
@@ -49,14 +56,24 @@ for(topo_name in topo_list) {
   }
 
 
-  ## TODO: GET UNIQUE STEADY STATES
+  ## GET UNIQUE STEADY STATES
   ss_unique_fname <- file.path(dataDir,"ss_unique.Rds")
   racipe_summary_fname <- file.path(dataDir,"racipe_summary.Rds")
   if(!file.exists(ss_unique_fname)) {
+    states <- round(as.data.frame(t(assay(racipe))), n_places_rounded)
+    states$Model <- rep(1:nModels, each=nICs)
+    ss_unique <- states %>%
+      group_by(Model) %>%
+      distinct(across(-Model), .keep_all = TRUE) %>%
+      mutate(State = row_number()) %>%
+      ungroup()
 
+    racipe_summary_df <- unique_states %>%
+      group_by(Model) %>%
+      summarise(NumStates = n(), .groups = "drop")
 
-    #saveRDS(ss_unique, ss_unique_fname)
-    #saveRDS(racipe_summary_df, racipe_summary_fname)
+    saveRDS(ss_unique, ss_unique_fname)
+    saveRDS(racipe_summary_df, racipe_summary_fname)
   } else {
     ss_unique <- readRDS(ss_unique_fname)
     racipe_summary_df <- readRDS(racipe_summary_fname)
@@ -64,6 +81,7 @@ for(topo_name in topo_list) {
 
   ## NORMALIZATION
   ## TODO: adjust normalization to only focus on unique steady states
+  ## TODO: save a RACIPE object with unique steady states as ICs for later sims?
   genes <- rownames(racipe)
   unnormData <- t(assay(racipe))
   simExp <- assay(racipe, 1)
